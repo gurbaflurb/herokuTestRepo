991e31410302f0c8e399fd565f73fe0f59e52602
Sean Chen <seachen4411@gmail.com>
2019-03-18 01:31:53 -0600

    Test Changes
    Changed a few files

--- app/models/add_flight.rb
+++ app/models/add_flight.rb
@@ -1,2 +1,15 @@
 class AddFlight < ApplicationRecord
+	validates :departing, presence: true
+	validates :arriving, presence: true
+	validates :departureTime, presence: true
+	validates :arrivalTime, presence: true
+	validate :arrivalTime_not_before_departureTime
+
+	def arrivalTime_not_before_departureTime
+		return if departureTime.nil? || arrivalTime.nil?
+
+		if arrivalTime < departureTime
+			errors.add(:to, "Can't arrive before you leave!")
+		end
+	end
 end
